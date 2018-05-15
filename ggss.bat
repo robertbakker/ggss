dfd0962ddf9d773a5e3859bf7ab646666335ede5
Robert Bakker <r.m.bakker@live.nl>
2018-05-15 19:44:57 +0200

    Second commit

--- ggss.bat
+++ ggss.bat
@@ -1,25 +1,9 @@
 @echo off
-set c=%~dp0swagger-config.json
 set i=swagger.yml
-set d=0
 
-:initial
-if "%1"=="" goto done
-set aux=%1
-if "%aux:~0,1%"=="-" (
-   set nome=%aux:~1,250%
-) else (
-   set "%nome%=%1"
-   set nome=
-)
-shift
-goto initial
-:done
-
-set SCRIPT_PATH=%~dp0
-set CURRENT_PATH=%cd%
+set v=%1
 set DEBUG_PARAMS=
-if "%d%"=="1" (
+if "%v%"=="1" (
     set DEBUG_PARAMS=-DdebugModels -DdebugOperations
 )
-java %DEBUG_PARAMS% -jar %SCRIPT_PATH%/bin/swagger-codegen-cli.jar generate --ignore-file-override %~dp0/.swagger-codegen-ignore -o web -i %i% -c %c% -l go-server -t  %SCRIPT_PATH%/swagger-templates
+java %DEBUG_PARAMS% -jar %~dp0bin\swagger-codegen-cli.jar generate --ignore-file-override %~dp0.swagger-codegen-ignore -o web -i swagger.yml -c %~dp0swagger-config.json -l go-server -t  %~dp0swagger-templates
